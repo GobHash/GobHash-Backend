7b08af6b2df9e8307c8180fe594c21798b628c5b
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-05-18 13:24:49 -0600

    init maligun service

---
+++ server/v1/helpers/email.js
@@ -0,0 +1,20 @@
+import config from '../../../config/config';
+
+const apiKey = config.mailgun_key;
+const domain = 'newtonlabs.com.gt';
+const mailgun = require('mailgun-js')({ apiKey, domain });
+
+const preData = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'serobnic@mail.ru',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomness!'
+};
+
+const sendEmail = (data = preData) => {
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body); //eslint-disable-line
+  });
+};
+
+export default { sendEmail };
