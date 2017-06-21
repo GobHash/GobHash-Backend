afd03fa3cff1caff0ef6b60b16152415089313d8
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-06-21 13:05:41 -0600

    reorder import

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // config should be imported before importing any other file
 import polyfill from 'babel-polyfill'; // eslint-disable-line
-import pmx from 'pmx';
-import config from './config/config';
-import app from './config/express';
+import pmx from 'pmx';                 // eslint-disable-line
+pmx.init({ http: true });              // eslint-disable-line enable http keymetris
+import config from './config/config';  // eslint-disable-line
+import app from './config/express';    // eslint-disable-line
 
-pmx.init({ http: true }); // enable http keymetris
 
 const debug = require('debug')('express-mongoose-es6-rest-api:index');
 
