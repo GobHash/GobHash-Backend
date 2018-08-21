3754e75b2ba87f6068ec9fc0db98ba33399e7440
Pablo Díaz <pablo5_diaz@hotmail.com>
2018-08-20 22:36:47 -0600

    remove .env from gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ const plugins = gulpLoadPlugins();
 
 const paths = {
   js: ['./**/*.js', '!dist/**', '!node_modules/**', '!coverage/**', '!react_docs/**'],
-  nonJs: ['./package.json', './.gitignore', './.env'],
+  nonJs: ['./package.json', './.gitignore'],
   tests: './server/tests/*.js'
 };
 
